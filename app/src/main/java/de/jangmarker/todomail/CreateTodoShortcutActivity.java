a4d5a30d43ccdf6d62628a239625302e557e219b
Jan Marker <jan@jangmarker.de>
2016-10-17 23:55:21 +0200

    Open new mail activity with correct mail address

--- app/src/main/java/de/jangmarker/todomail/CreateTodoShortcutActivity.java
+++ app/src/main/java/de/jangmarker/todomail/CreateTodoShortcutActivity.java
@@ -3,6 +3,7 @@ package de.jangmarker.todomail;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.Intent.ShortcutIconResource;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class CreateTodoShortcutActivity extends Activity {
@@ -11,17 +12,16 @@ public class CreateTodoShortcutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Intent createTodoActivity = new Intent(this, CreateTodoShortcutActivity.class);
-        // TODO #10 implement logic
+        final Intent createShortcutIntent = new Intent();
 
+        final Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + "todo@jangmarker.de"));
         final ShortcutIconResource iconResource = ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher);
 
-        final Intent createShortcutIntent = new Intent();
-        createShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, createTodoActivity);
+        createShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, emailIntent);
         createShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, "Create Todo");
         createShortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
-        setResult(RESULT_OK, createShortcutIntent);
 
+        setResult(RESULT_OK, createShortcutIntent);
         finish();
     }
 
