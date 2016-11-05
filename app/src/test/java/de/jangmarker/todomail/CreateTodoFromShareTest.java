e5d04ef11a91f291e631612c9edae00ea4fc6280
Jan Marker <jan@jangmarker.de>
2016-11-05 17:15:32 +0100

    Test code paths for invalid received share intents

--- app/src/test/java/de/jangmarker/todomail/CreateTodoFromShareTest.java
+++ app/src/test/java/de/jangmarker/todomail/CreateTodoFromShareTest.java
@@ -2,6 +2,7 @@ package de.jangmarker.todomail;
 
 import android.content.Intent;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -11,29 +12,56 @@ import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowActivity;
 
 import static de.jangmarker.todomail.MailTodoIntentMatcher.mailTodoIntentWithSubject;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class CreateTodoFromShareTest {
-    private static String subject = "Hello World!";
+    private static String SUBJECT = "Hello World!";
 
-    private static Intent validIntent = new Intent(Intent.ACTION_SEND);
+    private Intent intent;
 
-    static {
-        validIntent.setType("text/plain");
-        validIntent.putExtra(Intent.EXTRA_TEXT, subject);
+    @Before
+    public void resetIntentToValidIntent() {
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, SUBJECT);
     }
 
     @Test
     public void withValidSubject_sendsMail() {
-        CreateTodoFromShare activity =
-                Robolectric.buildActivity(CreateTodoFromShare.class, validIntent)
-                        .create().start().visible().get();
-        ShadowActivity shadow = Shadows.shadowOf(activity);
+        final ShadowActivity shadow = startActivity(intent);
+        assertThat(shadow.peekNextStartedActivity(), mailTodoIntentWithSubject(SUBJECT));
+    }
 
-        assertThat(shadow.peekNextStartedActivity(), mailTodoIntentWithSubject(subject));
+    @Test
+    public void notSendAction_doNothing() {
+        intent.setAction("another action");
+        final ShadowActivity shadow = startActivity(intent);
+        assertThat(shadow.peekNextStartedActivity(), nullValue());
+    }
+
+    @Test
+    public void mimeTypeNotSet_doNothing() {
+        intent.setType(null);
+        final ShadowActivity shadow = startActivity(intent);
+        assertThat(shadow.peekNextStartedActivity(), nullValue());
+    }
+
+    @Test
+    public void mimeTypeNotString_doNothing() {
+        intent.setType("not text/plain");
+        final ShadowActivity shadow = startActivity(intent);
+        assertThat(shadow.peekNextStartedActivity(), nullValue());
+    }
+
+    public ShadowActivity startActivity(Intent intent) {
+        CreateTodoFromShare activity =
+                Robolectric.buildActivity(CreateTodoFromShare.class, intent)
+                        .create().start().visible().get();
+        return Shadows.shadowOf(activity);
     }
 
 }
